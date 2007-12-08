9241b2fe210276c4c73d39728be9a6731d9b5bbd
Lechdan@gmail.com <Lechdan@gmail.com@f4119ed2-7e3e-0410-843f-17f1c127d565>
2007-12-08 10:39:28 +0000

    Meine kleine Spielwiese für RMI Tests: - RMI Aufbau gedöhns - Serialisiertes Objekt hin und her geschoben !! - Threads eingebaut

---
+++ trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/spielwiese/RMITestServer.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package spielwiese;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+
+/**
+ * @author <a href="mailto:reichert.sascha@googlemail.com">Sascha Reichert, reichert.sascha@googlemail.com</a>
+ *
+ */
+public class RMITestServer {
+
+	/**
+	 * Standard Main Methode zum Starten des Servers 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String host = "localhost"; 
+		if (args.length != 0){
+			if (args[0]!= null)
+			{
+				host = args[0]; 
+			}
+		} 
+
+	    try {
+		      LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+		    }	    
+		    catch (RemoteException ex) {
+		      System.out.println(ex.getMessage());
+		    }
+		    try {
+		      Naming.rebind("rmi://"+host+"/server", new RMITestImpl());
+		    }
+		    catch (MalformedURLException ex) {
+		      System.out.println(ex.getMessage());
+		    }
+		    catch (RemoteException ex) {
+		      System.out.println(ex.getMessage());
+		      ex.printStackTrace(); 
+		    }
+	}
+
+}
