498d8ec02debf727872edf0bc8bb6635034e2b50
Lechdan@gmail.com <Lechdan@gmail.com@f4119ed2-7e3e-0410-843f-17f1c127d565>
2007-12-08 10:39:28 +0000

    Meine kleine Spielwiese für RMI Tests: - RMI Aufbau gedöhns - Serialisiertes Objekt hin und her geschoben !! - Threads eingebaut

---
+++ verteiltesysteme0708/Verteilte Systeme Projekt/src/spielwiese/RMITestThread.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package spielwiese;
+
+import java.util.Date;
+import java.util.Vector;
+
+/**
+ * @author <a href="mailto:reichert.sascha@googlemail.com">Sascha Reichert,
+ *         reichert.sascha@googlemail.com</a>
+ * 
+ */
+public class RMITestThread extends Thread {
+
+	private Vector objectList;
+	private int personNumber; 
+	private RMITestObject returnObject;
+
+	/**
+	 * Konstruktor 
+	 * @param objectList
+	 * @param personNumber
+	 */
+	public RMITestThread(Vector objectList, int personNumber){
+		this.objectList = objectList; 
+		this.personNumber = personNumber; 
+	}
+	
+	/**
+	 * @return the returnObject
+	 */
+	public RMITestObject getReturnObject() {
+		return returnObject;
+	}
+
+	/**
+	 * @param objectList
+	 *            the objectList to set
+	 */
+	public void setObjectList(Vector objectList) {
+		this.objectList = objectList;
+	}
+
+	/**
+	 * Runmethode des Threads 
+	 */
+	public void run() {
+		Date dt = new Date();
+		System.out.println(dt.toString() + ": Thread started");
+		
+		try {
+			sleep(5000);
+		} catch (InterruptedException e) {
+		}
+		for (int i = 0; i < this.objectList.size(); i++)
+		{
+			if (i == this.personNumber)
+			
+				this.returnObject = (RMITestObject)this.objectList.get(i);
+		}
+		dt = new Date();
+		System.out.println(dt.toString() + ": Thread ended");
+	}
+
+}
