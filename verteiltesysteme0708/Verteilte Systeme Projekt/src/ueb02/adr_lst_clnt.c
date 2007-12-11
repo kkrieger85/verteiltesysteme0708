a2b310094cd2c310d46dde6071ab7e45f49b5cdb
Andreas.Bauer.80 <Andreas.Bauer.80@f4119ed2-7e3e-0410-843f-17f1c127d565>
2007-12-11 10:32:09 +0000

    Small changes. Mainly makefile, compiles now with C++

--- verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb02/adr_lst_clnt.c
+++ verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb02/adr_lst_clnt.c
@@ -3,46 +3,53 @@
  * It was generated using rpcgen.
  */
 
+#include <memory.h> /* for memset */
 #include "adr_lst.h"
 
 /* Default timeout can be changed using clnt_control() */
 static struct timeval TIMEOUT = { 25, 0 };
 
 int *
-adr_lst_auth_1(argp, clnt)
-	adr_lst_auth_data *argp;
-	CLIENT *clnt;
+adr_lst_auth_1(adr_lst_auth_data *argp, CLIENT *clnt)
 {
 	static int clnt_res;
 
 	memset((char *)&clnt_res, 0, sizeof(clnt_res));
-	if (clnt_call(clnt, ADR_LST_AUTH, xdr_adr_lst_auth_data, argp, xdr_int, &clnt_res, TIMEOUT) != RPC_SUCCESS)
+	if (clnt_call (clnt, ADR_LST_AUTH,
+		(xdrproc_t) xdr_adr_lst_auth_data, (caddr_t) argp,
+		(xdrproc_t) xdr_int, (caddr_t) &clnt_res,
+		TIMEOUT) != RPC_SUCCESS) {
 		return (NULL);
+	}
 	return (&clnt_res);
 }
 
 adr_lst_query_result *
-adr_lst_query_1(argp, clnt)
-	int *argp;
-	CLIENT *clnt;
+adr_lst_query_1(int *argp, CLIENT *clnt)
 {
 	static adr_lst_query_result clnt_res;
 
 	memset((char *)&clnt_res, 0, sizeof(clnt_res));
-	if (clnt_call(clnt, ADR_LST_QUERY, xdr_int, argp, xdr_adr_lst_query_result, &clnt_res, TIMEOUT) != RPC_SUCCESS)
+	if (clnt_call (clnt, ADR_LST_QUERY,
+		(xdrproc_t) xdr_int, (caddr_t) argp,
+		(xdrproc_t) xdr_adr_lst_query_result, (caddr_t) &clnt_res,
+		TIMEOUT) != RPC_SUCCESS) {
 		return (NULL);
+	}
 	return (&clnt_res);
 }
 
 int *
-adr_lst_logoff_1(argp, clnt)
-	int *argp;
-	CLIENT *clnt;
+adr_lst_logoff_1(int *argp, CLIENT *clnt)
 {
 	static int clnt_res;
 
 	memset((char *)&clnt_res, 0, sizeof(clnt_res));
-	if (clnt_call(clnt, ADR_LST_LOGOFF, xdr_int, argp, xdr_int, &clnt_res, TIMEOUT) != RPC_SUCCESS)
+	if (clnt_call (clnt, ADR_LST_LOGOFF,
+		(xdrproc_t) xdr_int, (caddr_t) argp,
+		(xdrproc_t) xdr_int, (caddr_t) &clnt_res,
+		TIMEOUT) != RPC_SUCCESS) {
 		return (NULL);
+	}
 	return (&clnt_res);
 }
