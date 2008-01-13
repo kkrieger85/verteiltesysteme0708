97df63320f8b74a542423343fae8394dc26166d4
folz.matthias <folz.matthias@f4119ed2-7e3e-0410-843f-17f1c127d565>
2008-01-13 16:26:28 +0000

    mit Sleep(1) versucht zu Debuggen, kein Ergebnis...

--- trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb01/server.c
+++ trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb01/server.c
@@ -4,33 +4,33 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#include <netinet/in.h> // Benötigt für Internet Namensauflösung
-#include <unistd.h> 	// Benötigt fürs Forken
+#include <netinet/in.h> // Benoetigt fuer Internet Namensaufloesung
+#include <unistd.h> 	// Benoetigt fuers Forken
 #include "userData.h"
 
 /** ToDo:
  * ->	Daten zuruecksenden 
- * ->	Forken korregieren
+ * ->	Forken korregieren test
  */
 /** Erklaerung zu der Ausagebe des laufenden Programm:
  * 	Die Zahl an erster Stelle sagt welcher Fork er ist.
  * 
  * 	Warum Forkt nur der Prozess 0?
- *  Damit dieser die Forks entsprechend Zählen kann und
+ *  Damit dieser die Forks entsprechend Zaehlen kann und
  *  bei der Maximalzahl dicht macht
  */
 
 #define PORT 4242
-#define MAX_QUEUE 3		// Maximale Anzahl von wartenden CLients in der Listen-Queue
-#define BUF_SIZE 1024	// Maximale größe der übertragenden Nachricht
-#define FORKCOUNT 10   	// Ist dafür zustaendig das der Prozess sich maximal 10 mal Reproduziert
-int listen_sock, send_sock; // eigendliche Sockets
+#define MAX_QUEUE 3				// Maximale Anzahl von wartenden CLients in der Listen-Queue
+#define BUF_SIZE 1024			// Maximale groesse der uebertragenden Nachricht
+#define FORKCOUNT 10   			// Ist dafuer zustaendig das der Prozess sich maximal 10 mal Reproduziert
+int 	listen_sock, send_sock; // eigendliche Sockets
 
 struct sockaddr_in sockaddr_client;	// Adresse des anfragenden Clients
 struct sockaddr_in sockaddr_server; // Adresse des Servers
 
-int forking = 0;// Fork-Limit Variabel
-int fehler = 0;	// Fehlervariable
+int forking = 0;	// Fork-Limit Variabel, diese Gibt an welches Kind gerade am laufen ist
+int fehler	= 0;	// Fehlervariable
 
 // Benutzerdatenarray folgt:
 const struct userData daten[]	=	{{ "Hugo","Balder",123456,"testosteron"},
@@ -105,7 +105,11 @@ void gabeln() {
 		fehler = 2;
 		return ;
 	}
-	// Wenn Maximum erreicht, verbinde selbst "Sollte noch geändert werden,
+	
+	// eine Sekunde Schlafen um RaceConditions vorzubeugen
+	while( sleep(1) > 0){}
+	
+	// Wenn Maximum erreicht, verbinde selbst "Sollte noch geaendert werden,
 	// weil sonst server nach 10 Anfragen aus ist
 	if (forking >= FORKCOUNT) {
 		verbindeMitClient();
@@ -115,38 +119,42 @@ void gabeln() {
 		// Vater wartet weiter auf kommende Events
 		if (vater == 0)
 			aufVerbindungWarten();
-		// Kind kümmert sich um den Client
+		// Kind kï¿½mmert sich um den Client
 		else			
 			verbindeMitClient();
 	}
 }
 
 void verbindeMitClient() {
-	printf("%i-!> Verbindung öffnen...\n",forking);
+	printf("%i-!> Verbindung oeffnen...\n",forking);
 	char buffer[BUF_SIZE] = " ";	// Buffer mit der zu uebertragenden Nachricht	
 			
 	
-	// Empfang des Hänmdeschüttelns des Clients
+	// Empfang des Haenmdeschuettelns des Clients
 	printf("%i-?> Empfange Nachricht...\n",forking);
 	recv(send_sock, buffer, BUF_SIZE, 0); 
 	buffer[BUF_SIZE]= '\0';		// C-String finalisieren		
-	printf("%i-$> Erhielt:\t%s \n",forking, buffer);
+	printf("%i-$> Erhielt:\t%s \n",forking, buffer);	
+
 	
-	// wenn der Cleint Käse von sich gibt, wird hier Terminiert
+	// wenn der Cleint Kaese von sich gibt, wird hier Terminiert
 	if( strcmp( buffer, "Ja ich bin da." ) != 0 ){
 		printf("%i-!> verstehe Client nicht!",forking);
 		fehler = 4;
 		return ;
 	}
 	
-	// Senden der Bestätigung
+	// eine Sekunde Schlafen um RaceConditions vorzubeugen
+	while( sleep(1) > 0){}
+	
+	// Senden der Bestaetigung
 	char sendBuffer[BUF_SIZE] = "Ich hoehre...";
 	printf("%i-?> Sende:\t%s \n",forking, sendBuffer); 
 	send(send_sock, sendBuffer, strlen(sendBuffer), 0); 
 	
 	// nun kann der Client beginnen sich zu Authorisieren
 	// dem Benutzer werden 3 Logginversuche gestattet bis der 
-	// Socket geschloßen wird
+	// Socket geschlossen wird
 	for(int i = 0; (authorisiereUndSende()!= 0) &&(i < 3) ; i++){
 		printf("%i-!> loggin Daten falsch!",forking);
 		
@@ -157,7 +165,10 @@ void verbindeMitClient() {
 	return;
 }
 int authorisiereUndSende(){
-	char buffer[BUF_SIZE] = " ";	// Buffer mit der zu uebertragenden Nachricht				
+	char buffer[BUF_SIZE] = " ";	// Buffer mit der zu uebertragenden Nachricht		
+	
+	// eine Sekunde Schlafen um RaceConditions vorzubeugen
+	while( sleep(1) > 0){}
 		
 	// Empfang des Benutzernamens
 	printf("%i-?> Empfange Benutzername...\n",forking);
@@ -175,6 +186,9 @@ int authorisiereUndSende(){
 	// wenn nichts gefunden wurde ist d = i
 	if(d == i)
 		return 1;
+	
+	// eine Sekunde Schlafen um RaceConditions vorzubeugen
+	while( sleep(1) > 0){}
 	
 	// Empfang des Passwortes
 	printf("%i-?> Empfange Passwort...\n",forking);
