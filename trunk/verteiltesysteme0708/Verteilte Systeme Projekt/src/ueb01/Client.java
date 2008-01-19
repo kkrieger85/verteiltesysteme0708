6ce054beaf7fb3ed2d27e700d08b2c5d9d39761b
folz.matthias <folz.matthias@f4119ed2-7e3e-0410-843f-17f1c127d565>
2008-01-19 18:11:41 +0000

    Proof dingens eingebaut, Händeschütteln funktioniert neuer fehler vorhanden, steht in der server.c TODO

--- trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb01/Client.java
+++ trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb01/Client.java
@@ -3,12 +3,15 @@ package ueb01;
 import java.net.*;
 import java.io.*;
 
+import ueb01.*;
+
 
 public class Client {
 
 	BufferedReader empfangen;
-	PrintStream senden;
+	BufferedWriter senden;
 	Socket clientSocket;
+	String puffer;		// Wird zum auslesen der Rueckgaben verwendet
 
 	public Client(String ip) {
 		try {
@@ -16,27 +19,36 @@ public class Client {
 			clientSocket = new Socket(InetAddress.getByName(ip), 4242);
 			
 			// Klasse Socket besitzt Methoden
-			// getInputStream bzw. getOutputStream, hier
-			// Konversion zu DataInputStream / PrintStream:
+			// getInputStream bzw. getOutputStream
 			empfangen = new BufferedReader(
 						new InputStreamReader(clientSocket.getInputStream()));
-			senden = new PrintStream(clientSocket.getOutputStream());
+			senden = new BufferedWriter(
+					 new OutputStreamWriter(clientSocket.getOutputStream()));
 
-			// Händeschütteln mit dem Server starten
+			// Haendeschuetteln mit dem Server starten
 			System.out.println("?> Server bereit?"); 
-			senden.print("Ja ich bin da.");
+			senden.write("Ja ich bin da.\n");
+			senden.flush();
 			
-			// auf Bestätigung warten
-			if(empfangen.readLine() == "Ich hoehre..."){				
+			/* auf Bestaetigung warten,			
+			 * Es wird getrimt, weil der C-Server
+			 * einen String der gesammten
+			 * Bufferlaenge zurÃ¼ckgiebt
+			 */
+			puffer = empfangen.readLine().trim();
+			
+			if(puffer.equals("Ich hoehre...")){				
 				System.out.println("!> Server ist bereit!"); 
 				// Solange anmelden lassen bis Authorisiert
 				while(!einLoggen());
+				
 			}else{
 				// Server antwortet nicht, voll? anderer Server als gewollt?
-				System.out.println("!> Verbinden zu "+ip+" nicht möglich!");
+				System.out.println("!> Verbinden zu "+ip+" nicht moeglich!");
+				System.out.println("\t> er antwortete:"+ puffer);
 			}
 			
-			// Socket schließen, Programm beenden
+			// Socket schliessen, Programm beenden
 			clientSocket.close();			
 			System.exit(0);
 			
@@ -50,29 +62,74 @@ public class Client {
 	 * @return True wenn vom Server Authorisiert
 	 */
 	public boolean einLoggen() throws IOException{
-		// Benutzernamen eingeben
-		System.out.print("$> Benutzername:\t");
-		senden.println(Stdin.readString());
-		// Vom Server bestätigen lassen
-		if(empfangen.readLine() != "Stimmt!")
-			return false;
+		boolean autorisieren= false;
 		
-		// Passwort eingeben
-		System.out.print("$> Passwort(KLARTEXT!):\t");
-		senden.println(Stdin.readString());	
-		// Vom Server bestätigen lassen
-		if(empfangen.readLine() != "Stimmt!")
-			return false;
-		
+		while(!autorisieren){			//Laeuft solange bis Alles stimmt
+			while(!benutzer()){};		//Frage solange Benutzer stimmt nach Benutzername
+			autorisieren=passwort();	// Frage nach Passwort
+		}		
 		return true;
 	}	
 	
 	/**
+	 * Vereinfacht das 3 Fache Abfragen des Benutzernamens falls dieser Falsch ist
+	 * @return
+	 * @throws IOException
+	 */
+	private boolean benutzer() throws IOException{
+		// Benutzernamen eingeben	
+		System.out.print("$> Benutzername:\t");	
+		puffer = Stdin.readString();
+		
+		// Benutzernamen senden
+		senden.write(puffer.trim());
+		senden.newLine();
+		senden.flush();
+				
+		System.out.println("test");
+		// Vom Server bestaetigen lassen
+		puffer = empfangen.readLine().trim();			
+		System.out.println("test2"); // Hier kommt er nie an, obwohl server fertig sendet
+		
+		if(puffer.equals("Stimmt!"))
+			return true;
+		else if(puffer.equals("Falsch"))
+			return false;
+		else
+			throw new IOException(puffer);		
+	}
+	
+	/**
+	 * Vereinfacht das 3 Fache Abfragen des Passwortes falls dieser Falsch ist
+	 * @return
+	 * @throws IOException
+	 */
+	private boolean passwort() throws IOException{
+		// Passwort eingeben
+		System.out.print("$> Passwort(KLARTEXT!):\t");	
+		puffer = Stdin.readlnString();
+		
+		// Passwort senden
+		senden.write(puffer.trim());
+		senden.newLine();
+		senden.flush();
+				
+		// Vom Server bestaetigen lassen
+		puffer = empfangen.readLine().trim();	
+											
+		if(puffer.equals("Stimmt!"))
+			return true;
+		else if(puffer.equals("Falsch"))
+			return false;
+		else
+			throw new IOException(puffer);		
+	}	
+	/**
 	 * Mainmethode welche den Start des Clients anregt, und die IP des servers Festlegt
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// für schnelleres Testen feste ip.
+	public static void main(String args[]) {
+		// fï¿½r schnelleres Testen feste ip.
 		Client test = new Client("192.168.0.3");
 	}
 
--- trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb01/server.c
+++ trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb01/server.c
@@ -6,11 +6,13 @@
 
 #include <netinet/in.h> // Benoetigt fuer Internet Namensaufloesung
 #include <unistd.h> 	// Benoetigt fuers Forken
+#include <signal.h>		// Wird verwendet um herrauszukreigen wenn ein Kind beendet wurde
 #include "userData.h"
 
-/** ToDo:
+/** ToDo
  * ->	Daten zuruecksenden 
- * ->	Forken korregieren test
+ * ->	Dokumentation der Funktionen von Lars
+ * ->	Problem damit das Server Sendent aber Client ewig im Empfang feststeckt
  */
 /** Erklaerung zu der Ausagebe des laufenden Programm:
  * 	Die Zahl an erster Stelle sagt welcher Fork er ist.
@@ -29,12 +31,12 @@ int 	listen_sock, send_sock; // eigendliche Sockets
 struct sockaddr_in sockaddr_client;	// Adresse des anfragenden Clients
 struct sockaddr_in sockaddr_server; // Adresse des Servers
 
-int forking = 0;	// Fork-Limit Variabel, diese Gibt an welches Kind gerade am laufen ist
+int forking = 0;	// Fork Variabel, diese Gibt an welches Kind gerade am laufen ist
 int fehler	= 0;	// Fehlervariable
 
 // Benutzerdatenarray folgt:
-const struct userData daten[]	=	{{ "Hugo","Balder",123456,"testosteron"},
-									{ "Master","keks",4223,"keks"}};
+const struct userData daten[]	=	{{ "Hugo\n","Balder",123456,"testosteron\n"},
+									{ "Master\n","keks",4223,"keks\n"}};
 const int	datenGroesse		= 2;
 
 //Funktions-Deklarationen
@@ -42,13 +44,23 @@ void aufVerbindungWarten();
 void gabeln();
 void verbindeMitClient();
 int	 authorisiereUndSende();
+int  readline(register int fd, register char *ptr, register int maxlen) ;
+int  writen(register int fd, register char *ptr, register int nbytes);
 
-int main(int args, char *argv[]) {
+
+
+/**
+ * Main-Funktion,
+ * Bindet den Serversocket auf Adressen und regt das Lauschen an 
+ */
+int main(int args, char *argv[]) {	
+	
 	printf("%i->> initialisieren...\n",forking);
 
-	// Socket Initialisieren,
-	// PF_INET bedeutet das man die protokollfamilie des 
-	// Internet benutzen moechte
+	/* Socket Initialisieren,
+	 * PF_INET bedeutet das man die protokollfamilie des 
+	 * Internet benutzen moechte
+	 */
 	listen_sock = socket(PF_INET ,SOCK_STREAM , 0);
 	if (listen_sock < 0) {
 		printf("%i-!> Socket konnte nicht geoeffnet werden!", forking);
@@ -80,65 +92,73 @@ int main(int args, char *argv[]) {
 void aufVerbindungWarten() {
 	printf("%i-!> Auf Verbindung warten...\n",forking);	
 	
-	// Socket wird in Listeningmodus versetzt,
-	// dabei werden Maximal MAX_QUEUE viele hineingelassen
-	// ist die Queue voll gibt es einen Fehler
+	/* Socket wird in Listeningmodus versetzt,
+	 * dabei werden Maximal MAX_QUEUE viele hineingelassen
+	 * ist die Queue voll gibt es einen Fehler
+	 */
 	if (listen(listen_sock, MAX_QUEUE) == -1){	
 		printf("%i-!> listen () fehlgeschalgen!",forking);
 	    fehler = 3;
 	    return ;
 	}
-	//Forken anregen
-	gabeln();
-}
-
-void gabeln() {
-	int vater = 0;  // Temporaer fuer Debbiging Zwecke auskommentiert
-	printf("%i-!> Horchen...\n",forking);	
-	forking++;
 	
-	int inAddr = sizeof(sockaddr_client);		// Groesse der Clientadresse	
+	printf("%i-!> Horchen...\n",forking);	
+	
+	int inAddr = sizeof(sockaddr_client);	// Groesse der Clientadresse	
+	
 	// Sendesocket erstellen, ab jetzt wartet das Socket aktiv.
 	send_sock =	accept(listen_sock, (struct sockaddr *) &sockaddr_client, &inAddr ); 
 	if( send_sock < 0 ){
 		printf("%i-!> Verbindung konnte nicht hergestellt werden!",forking);
 		fehler = 2;
 		return ;
-	}
+	}	
+	
+	// Client will verbinden, Forken anregen
+	gabeln();
+}
+
+void gabeln() {
+	int vater = 0;  // Identifiziert ob der Prozess Vater oder schon Kind is
+	forking++;		// Forking wird erhoeht
 	
 	// eine Sekunde Schlafen um RaceConditions vorzubeugen
 	while( sleep(1) > 0){}
 	
-	// Wenn Maximum erreicht, verbinde selbst "Sollte noch geaendert werden,
-	// weil sonst server nach 10 Anfragen aus ist
+	/* Wenn Maximum erreicht, verbinde selbst "Sollte noch geaendert werden,
+	 * weil sonst server nach 10 Anfragen aus ist
+	 */
 	if (forking >= FORKCOUNT) {
 		verbindeMitClient();
 	}	
 	else {
 		vater = fork();
-		// Vater wartet weiter auf kommende Events
+		
+		// Kind kuemmert sich um den Client		
 		if (vater == 0)
-			aufVerbindungWarten();
-		// Kind k�mmert sich um den Client
-		else			
 			verbindeMitClient();
+		// Vater wartet weiter auf kommende Events
+		else{
+			forking--;
+			aufVerbindungWarten();	
+		}
 	}
 }
 
 void verbindeMitClient() {
 	printf("%i-!> Verbindung oeffnen...\n",forking);
-	char buffer[BUF_SIZE] = " ";	// Buffer mit der zu uebertragenden Nachricht	
+	char buffer[BUF_SIZE] = "";	// Buffer mit der zu uebertragenden Nachricht	
 			
 	
 	// Empfang des Haenmdeschuettelns des Clients
 	printf("%i-?> Empfange Nachricht...\n",forking);
-	recv(send_sock, buffer, BUF_SIZE, 0); 
-	buffer[BUF_SIZE]= '\0';		// C-String finalisieren		
+	readline(send_sock, buffer, BUF_SIZE); 
+	//buffer[BUF_SIZE]= '\0';		// C-String finalisieren		
 	printf("%i-$> Erhielt:\t%s \n",forking, buffer);	
 
 	
 	// wenn der Cleint Kaese von sich gibt, wird hier Terminiert
-	if( strcmp( buffer, "Ja ich bin da." ) != 0 ){
+	if( strcmp( buffer, "Ja ich bin da.\n" ) != 0 ){
 		printf("%i-!> verstehe Client nicht!",forking);
 		fehler = 4;
 		return ;
@@ -148,31 +168,40 @@ void verbindeMitClient() {
 	while( sleep(1) > 0){}
 	
 	// Senden der Bestaetigung
-	char sendBuffer[BUF_SIZE] = "Ich hoehre...";
+	char sendBuffer[] = "Ich hoehre...\n";
 	printf("%i-?> Sende:\t%s \n",forking, sendBuffer); 
 	send(send_sock, sendBuffer, strlen(sendBuffer), 0); 
 	
-	// nun kann der Client beginnen sich zu Authorisieren
-	// dem Benutzer werden 3 Logginversuche gestattet bis der 
-	// Socket geschlossen wird
+	/* nun kann der Client beginnen sich zu Authorisieren
+	 * dem Benutzer werden 3 Logginversuche gestattet bis der 
+	 * Socket geschlossen wird
+	 */
 	for(int i = 0; (authorisiereUndSende()!= 0) &&(i < 3) ; i++){
 		printf("%i-!> loggin Daten falsch!",forking);
-		
+
+		// Sage Client das die Daten nicht Stimmen
+		char sendBuffer[] = "Falsch\n";
+		send(send_sock, sendBuffer, strlen(sendBuffer), 0); 		
 	}				
+	
+	// Sage Client das genug Versucht wurde
+	char ende[] = "Genug, das reicht! Zuviele Versuche\n";
+	send(send_sock, ende, strlen(ende), 0); 		
 	
 	// wenn der Durchlauf hier endet ist was Schief gegangen
 	fehler = 5;
 	return;
 }
 int authorisiereUndSende(){
-	char buffer[BUF_SIZE] = " ";	// Buffer mit der zu uebertragenden Nachricht		
+	char buffer[BUF_SIZE] = "";	// Buffer mit der zu uebertragenden Nachricht		
+	char bestaetigt[]	= "Stimmt!\n";
 	
 	// eine Sekunde Schlafen um RaceConditions vorzubeugen
 	while( sleep(1) > 0){}
 		
 	// Empfang des Benutzernamens
 	printf("%i-?> Empfange Benutzername...\n",forking);
-	recv(send_sock, buffer, BUF_SIZE, 0); 
+	readline(send_sock, buffer, BUF_SIZE);  // Warum wird der server nicht fertig mit dem empfangen?
 	buffer[BUF_SIZE]= '\0';		// C-String finalisieren		
 	printf("%i-$> Erhielt:\t%s \n",forking, buffer);
 	
@@ -183,22 +212,31 @@ int authorisiereUndSende(){
 		if(strcmp(daten[i].name, buffer) !=0)
 			d++;			
 	}
-	// wenn nichts gefunden wurde ist d = i
+	// wenn nichts gefunden wurde ist d = i, aslo wird Abgebrochen
 	if(d == i)
-		return 1;
+		return 1;	
+	
+	// eine Sekunde Schlafen um RaceConditions vorzubeugen
+	while( sleep(1) > 0){}
+	
+	// Sage Client das die Daten Stimmen
+	send(send_sock, bestaetigt,strlen(bestaetigt), 0); 
 	
 	// eine Sekunde Schlafen um RaceConditions vorzubeugen
 	while( sleep(1) > 0){}
 	
 	// Empfang des Passwortes
 	printf("%i-?> Empfange Passwort...\n",forking);
-	recv(send_sock, buffer, BUF_SIZE, 0); 
+	readline(send_sock, buffer, BUF_SIZE); 
 	buffer[BUF_SIZE]= '\0';		// C-String finalisieren		
 	printf("%i-$> Erhielt:\t%s \n",forking, buffer);
 	
-	// Passwort vergleichen
+	// Passwort vergleichen, wenn es nicht stimmt, wird Abgebrochen
 	if(strcmp(daten[i].passwort, buffer) !=0)
-		return 2;
+		return 2;	
+
+	// Sage Client das die Daten Stimmen
+	send(send_sock, bestaetigt,strlen(bestaetigt), 0); 
 	
 	// Hier muss noch das senden der Daten an den Client hin
 	
@@ -207,3 +245,38 @@ int authorisiereUndSende(){
 	exit(0);
 }
 
+// DOKU!!!!!
+// Umsetzung von Charweisem lesen zu zeilenweisem Lesen
+int readline(register int fd, register char *ptr, register int maxlen) {
+	int n, rc;
+	char c;
+	for (n = 1; n < maxlen; n++) {
+		if ( (rc = read(fd, &c, 1)) == 1) {
+			*ptr++ = c;
+			if (c == '\n')
+				break;
+		} else if (rc == 0) {
+			if (n == 1)
+				return (0); /* EOF, no data read */
+			else
+				break; /* EOF, some data was read */
+		} else
+			return (-1); /* error */
+	}
+	*ptr = 0;
+	return (n);
+}
+
+// Umsezung des Sendens an Client
+int writen(register int fd, register char *ptr, register int nbytes) {
+	int nleft, nwritten;
+	nleft = nbytes;
+	while (nleft > 0) {
+		nwritten = write(fd, ptr, nleft);
+		if (nwritten <= 0)
+			return (nwritten); /* error */
+		nleft -= nwritten;
+		ptr += nwritten;
+	}
+	return (nbytes - nleft);
+}
