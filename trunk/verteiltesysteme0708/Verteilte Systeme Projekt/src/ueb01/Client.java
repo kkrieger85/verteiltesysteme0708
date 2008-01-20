c764a8c06fa5f94c0bfb8cd8542e890bc25f473b
folz.matthias <folz.matthias@f4119ed2-7e3e-0410-843f-17f1c127d565>
2008-01-20 20:07:02 +0000

    Mit serverinterkation

--- trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb01/Client.java
+++ trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/ueb01/Client.java
@@ -1,143 +1,185 @@
 package ueb01;
 
 import java.net.*;
-import java.io.*; 
+import java.io.*;
 
 import ueb01.*;
-
 
 public class Client {
 
 	BufferedReader empfangen;
 	BufferedWriter senden;
 	Socket clientSocket;
-	String puffer;		// Wird zum auslesen der Rueckgaben verwendet
+	String puffer; // Wird zum auslesen der Rueckgaben verwendet
 	final static int BUF_SIZE = 1024;
-	
+
 	public Client(String ip) {
 		try {
-			System.out.println("!> Verbinde zu "+ip);
+			System.out.println("!> Verbinde zu " + ip);
 			clientSocket = new Socket(InetAddress.getByName(ip), 4242);
-			
+
 			// Klasse Socket besitzt Methoden
 			// getInputStream bzw. getOutputStream
-			empfangen = new BufferedReader(
-						new InputStreamReader(clientSocket.getInputStream()));
-			senden = new BufferedWriter(
-					 new OutputStreamWriter(clientSocket.getOutputStream()));
+			empfangen = new BufferedReader(new InputStreamReader(clientSocket
+					.getInputStream()));
+			senden = new BufferedWriter(new OutputStreamWriter(clientSocket
+					.getOutputStream()));
 
 			// Haendeschuetteln mit dem Server starten
-			System.out.println("?> Server bereit?"); 
+			System.out.println("?> Server bereit?");
 			senden.write("Ja ich bin da.\n");
 			senden.flush();
-			
-			/* auf Bestaetigung warten,			
-			 * Es wird getrimt, weil der C-Server
-			 * einen String der gesammten
-			 * Bufferlaenge zurueckgiebt
+
+			/*
+			 * auf Bestaetigung warten, Es wird getrimt, weil der C-Server einen
+			 * String der gesammten Bufferlaenge zurueckgiebt
 			 */
 			puffer = empfangen.readLine().trim();
-			
-			if(puffer.equals("Ich hoehre...")){				
-				System.out.println("!> Server ist bereit!"); 
+
+			if (puffer.equals("Ich hoehre...")) {
+				System.out.println("!> Server ist bereit!");
 				// Solange anmelden lassen bis Authorisiert
-				while(!einLoggen());
-				
+				while (!einLoggen())
+					;
+
 				// Empfange Benutzerdaten
-				System.out.println("!> Anmeldung erfolgreich"); 
-				System.out.println("\tNachname:\t"+ empfangen.readLine().trim());
-				System.out.println("\tKundenNr:\t"+ empfangen.readLine().trim());
+				System.out.println("!> Anmeldung erfolgreich");
 				
-			}else{
+				// Serverinteraktion starten
+				interaktion();
+
+			} else {
 				// Server antwortet nicht, voll? anderer Server als gewollt?
-				System.out.println("!> Verbinden zu "+ip+" nicht moeglich!");
-				System.out.println("\t> er antwortete:"+ puffer);
+				System.out
+						.println("!> Verbinden zu " + ip + " nicht moeglich!");
+				System.out.println("\t> er antwortete:" + puffer);
 			}
-			
+
 			// Socket schliessen, Programm beenden
-			
-			clientSocket.close();	
-			System.out.println("!> Verbindung Beendet"); 
+
+			clientSocket.close();
+			System.out.println("!> Verbindung Beendet!");
 			System.exit(0);
-			
+
 		} catch (Exception e) {
-			System.out.println("!> Fehler:" +e.getMessage());
+			System.out.println("!> Fehler:" + e.getMessage());
 		}
 	}
 
 	/**
 	 * Diese Methode dient zum interaktiven einloggen auf dem Server
+	 * 
 	 * @return True wenn vom Server Authorisiert
 	 */
-	public boolean einLoggen() throws IOException{
-		boolean autorisieren= false;
-		
-		while(!autorisieren){			//Laeuft solange bis Alles stimmt
-			while(!benutzer()){};		//Frage solange Benutzer stimmt nach Benutzername
-			autorisieren=passwort();	// Frage nach Passwort
-		}		
+	public boolean einLoggen() throws IOException {
+		boolean autorisieren = false;
+
+		while (!autorisieren) { // Laeuft solange bis Alles stimmt
+			while (!benutzer()) {
+			}
+			; // Frage solange Benutzer stimmt nach Benutzername
+			autorisieren = passwort(); // Frage nach Passwort
+		}
 		return true;
-	}	
-	
+	}
+
 	/**
-	 * Vereinfacht das 3 Fache Abfragen des Benutzernamens falls dieser Falsch ist
+	 * Vereinfacht das 3 Fache Abfragen des Benutzernamens falls dieser Falsch
+	 * ist
+	 * 
 	 * @return
 	 * @throws IOException
 	 */
-	private boolean benutzer() throws IOException{
-		// Benutzernamen eingeben	
-		System.out.print("$> Benutzername:\t");	
+	private boolean benutzer() throws IOException {
+		// Benutzernamen eingeben
+		System.out.print("$> Benutzername:\t");
 		puffer = Stdin.readString();
-		
+
 		// Benutzernamen senden
-		senden.write(puffer.trim()+"\n");
-		//senden.newLine();
+		senden.write(puffer.trim() + "\n");
+		// senden.newLine();
 		senden.flush();
-		
+
 		// Vom Server bestaetigen lassen
-		puffer = empfangen.readLine().trim();			
-		
-		if(puffer.equals("Stimmt!"))
+		puffer = empfangen.readLine().trim();
+
+		if (puffer.equals("Stimmt!"))
 			return true;
-		else if(puffer.equals("Falsch"))
+		else if (puffer.equals("Falsch"))
 			return false;
 		else
-			throw new IOException(puffer);		
+			throw new IOException(puffer);
 	}
-	
+
 	/**
 	 * Vereinfacht das 3 Fache Abfragen des Passwortes falls dieser Falsch ist
+	 * 
 	 * @return
 	 * @throws IOException
 	 */
-	private boolean passwort() throws IOException{
+	private boolean passwort() throws IOException {
 		// Passwort eingeben
-		System.out.print("$> Passwort(KLARTEXT!):\t");	
+		System.out.print("$> Passwort(KLARTEXT!):\t");
 		puffer = Stdin.readlnString();
-		
+
 		// Passwort senden puffer.trim()
-		senden.write(puffer.trim()+"\n");
-		//senden.newLine();
+		senden.write(puffer.trim() + "\n");
+		// senden.newLine();
 		senden.flush();
-				
+
 		// Vom Server bestaetigen lassen
-		puffer = empfangen.readLine().trim();	
-											
-		if(puffer.equals("Stimmt!"))
+		puffer = empfangen.readLine().trim();
+
+		if (puffer.equals("Stimmt!"))
 			return true;
-		else if(puffer.equals("Falsch"))
+		else if (puffer.equals("Falsch"))
 			return false;
 		else
-			throw new IOException(puffer);		
-	}		
-	
+			throw new IOException(puffer);
+	}
+
 	/**
-	 * Mainmethode welche den Start des Clients anregt, und die IP des servers Festlegt
+	 * Benutzen der Serverinteraktion
+	 * 
+	 * @throws IOException
+	 */
+	private void interaktion() throws IOException {
+		boolean beenden = false;
+
+		while (!beenden) {
+			System.out.println("0: Beenden\n1: Daten ausgeben");
+
+			// Einlesen der Parameter
+			System.out.print("$> Auswahl:\t");
+			puffer = Stdin.readlnString();
+
+			// Parameter senden puffer.trim()
+			senden.write(puffer.trim() + "\n");
+			senden.flush();
+
+			if (puffer.trim().equals("0"))
+				// Vom Server bestaetigen lassen
+				if (empfangen.readLine().trim().equals("Ende"))
+					beenden = true;
+				else
+					throw new IOException("Server Reagiert nicht!");
+			else if (puffer.trim().equals("1")){
+				System.out.println("\tNachname:\t"+ empfangen.readLine().trim());
+				System.out.println("\tKundenNr:\t"+ empfangen.readLine().trim());				
+			}
+
+		}
+	}
+
+	/**
+	 * Mainmethode welche den Start des Clients anregt, und die IP des servers
+	 * Festlegt
+	 * 
 	 * @param args
 	 */
 	public static void main(String args[]) {
 		// f�r schnelleres Testen feste ip.
-		Client test = new Client("192.168.0.3");
+		Client test = new Client("127.0.0.1");
 	}
 
 }
