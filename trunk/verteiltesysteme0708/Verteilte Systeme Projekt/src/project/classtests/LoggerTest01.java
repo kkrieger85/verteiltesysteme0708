42880429c809774504e87f3474893e1dcc18d66b
lechdan@gmail.com <lechdan@gmail.com@f4119ed2-7e3e-0410-843f-17f1c127d565>
2008-02-10 17:07:55 +0000

    Log4J Wrapper + Tests

---
+++ trunk/verteiltesysteme0708/Verteilte Systeme Projekt/src/project/classtests/LoggerTest01.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package project.classtests;
+
+
+/**
+ * @author <a href="mailto:reichert.sascha@googlemail.com">Sascha Reichert, reichert.sascha@googlemail.com</a>
+ *
+ */
+public class LoggerTest01 {
+
+	/**
+	 * 
+	 */
+	public LoggerTest01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		// Aufruf einer Testklasse und entsprechende Logausgaben generieren!!
+		LoggerTestClass ltc = new LoggerTestClass(); 
+		ltc.automatisierterTest(); 
+	}
+	
+
+
+}
