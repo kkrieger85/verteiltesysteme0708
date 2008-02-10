44146c89301d64ce3730acf1d5ef78bfba0864fb
lechdan@gmail.com <lechdan@gmail.com@f4119ed2-7e3e-0410-843f-17f1c127d565>
2008-02-10 17:07:55 +0000

    Log4J Wrapper + Tests

---
+++ verteiltesysteme0708/Verteilte Systeme Projekt/src/project/helperclasses/DDLogger.java
@@ -0,0 +1,113 @@
+/**
+ * 
+ */
+package project.helperclasses;
+
+import org.apache.log4j.*; 
+
+/**
+ * @author <a href="mailto:reichert.sascha@googlemail.com">Sascha Reichert, reichert.sascha@googlemail.com</a>
+ *
+ */
+public class DDLogger {
+
+	private static Logger log = Logger.getRootLogger();
+	public static Level ALL = Level.ALL; 
+	public static Level DEBUG = Level.DEBUG; 
+	public static Level INFO = Level.INFO; 
+	public static Level WARN = Level.WARN; 
+	public static Level ERROR = Level.ERROR; 
+	public static Level FATAL = Level.FATAL; 
+	public static Level OFF = Level.OFF; 
+		
+	/**
+	 * C'tor 
+	 * Vereinfachter Konstruktor mit Standardeinstellungen!! 
+	 */
+	public DDLogger(){
+		this(null, null); 
+		
+	}
+	
+	/**
+	 * C'tor 
+	 * Konstruktor zum explizieten Anlegen des Loglevels 
+	 * @param lvl Level der geloggt werden soll 
+	 */
+	public DDLogger(Level lvl){
+		this(lvl, null);
+	}
+	
+	/**
+	 * C'tor 
+	 * Klasse mit einer bestimmten Logdatei anlegen 
+	 * @param logfile String 
+	 */
+	public DDLogger(String logfile){
+		this(null,logfile); 
+	}
+	
+	/**
+	 * C'tor 
+	 * Dient zum Einstellen des Loggers, es gibt ne Standardeinstellung !!! 
+	 * @param lvl Der Level des Loggers 
+	 * @param logfile Die Datei in die geschrieben werden soll 
+	 */
+	public DDLogger(Level lvl, String logfile){
+		/// Logger Bekommt als Standardeinstellung ALL falls nichts übergeben wurde 
+		if (lvl == null){
+			lvl = ALL; 
+		}
+		// Logfile kann angegeben werden Muss aber nicht !!! 	
+		if (logfile == null){
+			logfile = "default.log"; 
+		}
+	    try {
+	    	// Alle Appender vorher Löschen!!! Es könnten sonst noch welche drin sein die dort nicht sein sollen. 
+	    	log.removeAllAppenders(); 
+	    	PatternLayout layout = new PatternLayout( "%d{ISO8601} %-5p [%t] %c: %m%n" );     
+		    // Es wird immer der ConsolenAppender und der Dateiappender benutzt !!! Bei Bedarf erweiterbar !!
+		    ConsoleAppender consoleAppender = new ConsoleAppender( layout );
+		    log.addAppender( consoleAppender );
+		    FileAppender fileAppender = new FileAppender( layout, logfile, true );
+		    log.addAppender( fileAppender );
+		    // ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF:
+		    log.setLevel( lvl );
+		  } catch( Exception ex ) {
+		    System.out.println( ex );
+		  }
+		    
+	}
+	
+	/**
+	 * Dient zum Erstellen eines Logeintrages 
+	 * Der Level kann mit DDLogger.* angegeben werden!! 
+	 * @param msg Die Nachricht als String 
+	 * @param lvl 	Den level 
+	 * @return Boolean obs geklappt hat 
+	 */
+	public boolean createLog(String msg, Level lvl){		
+		if (msg == null){
+			msg = "No Message!!!"; 
+		}		
+		if (lvl.toInt() == DDLogger.ALL.toInt()){
+			DDLogger.log.debug(msg); 
+		}
+		if (lvl.toInt() == DDLogger.DEBUG.toInt()){
+			DDLogger.log.debug(msg); 
+		}		
+		if (lvl.toInt() == DDLogger.INFO.toInt()){
+			DDLogger.log.info(msg); 
+		}	
+		if (lvl.toInt() == DDLogger.WARN.toInt()){
+			DDLogger.log.warn(msg); 
+		}		
+		if (lvl.toInt() == DDLogger.ERROR.toInt()){
+			DDLogger.log.error(msg); 
+		}		
+		if (lvl.toInt() == DDLogger.FATAL.toInt()){
+			DDLogger.log.fatal(msg); 
+		}			
+		return true; 
+	}
+}
